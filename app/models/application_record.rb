9c40c93c38ef19526d0f6fe477e18ec013cf1196
Andrre Lima <andre.salatalima@gmail.com>
2024-09-16 22:13:01 +0200

    CRUD broadcast implementation

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
+
 end
